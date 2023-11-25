cad380d85c69440cff004d8c487717e683627406
bill6502 <tp6rup4286>
2023-11-25 23:28:25 +0800

    upload again

---
+++ VRChat/SuikaGame/Scripts/Button.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Button : UdonSharpBehaviour
+{
+    public Game game;
+    public string funcName;
+
+    private VRCPlayerApi LocalPlayer;
+
+    private void Start()
+    {
+        LocalPlayer = Networking.LocalPlayer;
+    }
+    public override void Interact()
+    {
+        game.SendCustomEvent(funcName);
+    }
+}
