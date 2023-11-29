680ceb54126c8b6094814eeaac130e0e55d3047d
bill6502 <tp6rup4286>
2023-11-29 16:32:54 +0800

    add Brain_20231129.

---
+++ VRChat/Demo/Scripts/20231129/PartPickup_20231129.cs
@@ -0,0 +1,78 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[RequireComponent(typeof(VRC_Pickup))]
+public class PartPickup_20231129 : UdonSharpBehaviour
+{
+    public Tip_20231129 tip;
+    public GameObject Position;
+
+    [Tooltip("Distance of Resetting position"), Range(0f, .2f)]
+    public float Distance = .1f;
+
+    [SerializeField]
+    private GameObject label;
+    [SerializeField]
+    private LineRenderer lineRenderer;
+    [SerializeField]
+    private Transform[] linePosition;
+
+    [HideInInspector]
+    public bool isInside = false;
+
+    [UdonSynced]
+    private Vector3 _position2;
+    private Rigidbody rig;
+    private VRCPlayerApi LocalPlayer;
+
+    private void Start()
+    {
+        //setting
+        rig = this.GetComponent<Rigidbody>();
+        LocalPlayer = Networking.LocalPlayer;
+
+        if (Networking.IsOwner(LocalPlayer, gameObject))
+            _position2 = linePosition[1].position - linePosition[0].position;
+    }
+
+    public override void OnDrop()
+    {
+        bool isOwner = Networking.IsOwner(LocalPlayer, gameObject);
+        if (!Position || !isOwner) return;
+
+        rig.useGravity = false;
+        rig.isKinematic = true;
+
+        //ResetPosition
+        if (!isInside) return;
+
+        transform.position = Position.transform.position;
+        transform.rotation = Position.transform.rotation;
+    }
+
+    private void Update()
+    {
+        if (!tip || linePosition.Length == 0 || !label) return;
+
+        label.SetActive(tip.isShowLabel);
+        lineRenderer.SetPosition(0, linePosition[0].position);
+        if (tip.isShowLabel)
+            lineRenderer.SetPosition(1, linePosition[0].position + _position2);
+        else
+            lineRenderer.SetPosition(1, linePosition[0].position);
+
+        label.transform.position = linePosition[0].position + _position2;
+
+        label.transform.LookAt(LocalPlayer.GetBonePosition(HumanBodyBones.Head));
+        label.transform.rotation *= Quaternion.AngleAxis(-90f, Vector3.up);
+
+        float distance = Vector3.Distance(Position.transform.position, transform.position);
+        if (distance > Distance)
+            isInside = false;
+        else
+            isInside = true;
+    }
+}
---
+++ VRChat/Demo/Scripts/20231129/Tip_20231129.cs
@@ -0,0 +1,58 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class Tip_20231129 : UdonSharpBehaviour
+{
+    public PartPickup_20231129 Target;
+    // public Animator TipAnimator;
+    // public string TipParameterName = "index";
+
+    [SerializeField]
+    private Image image;
+    [SerializeField]
+    private Color unclick = new Color(1f, 1f, 1f, 1f);
+    [SerializeField]
+    private Color clicked = new Color(0f, 1f, 0f, 1f);
+
+    [HideInInspector]
+    public bool isShowLabel = false;
+
+    private VRCPlayerApi LocalPlayer;
+
+    private void Start()
+    {
+        //checking
+        if (!Target || !image)
+            return;
+
+        //setting
+        LocalPlayer = Networking.LocalPlayer;
+    }
+
+    public override void Interact()
+    {
+        //Getting Owner
+        Networking.SetOwner(LocalPlayer, gameObject);
+
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(tip));
+    }
+
+    public void tip()
+    {
+        // if (Target.isInside) return;
+
+        // bool isResetPosition = Target.transform.position == Target.Position.transform.position && Target.transform.rotation == Target.Position.transform.rotation;
+        isShowLabel = !isShowLabel;
+        // TipAnimator.SetBool(TipParameterName, isShowLabel);
+
+        if (isShowLabel)
+            image.color = clicked;
+        else
+            image.color = unclick;
+    }
+}
