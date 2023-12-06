3f61d4c7dfd72c424cb0c430e2b48099255e40a7
bill6502 <tp6rup4286>
2023-12-06 12:54:51 +0800

    add new prefab object.

---
+++ VRChat/Demo/Scripts/STEREOTAXIC_handler.cs
@@ -0,0 +1,48 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class STEREOTAXIC_handler : UdonSharpBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private string ParameterName;
+    [SerializeField]
+    private Slider slider;
+
+    [UdonSynced, FieldChangeCallback(nameof(SetParameter))]
+    private float ParameterValue;
+    public float SetParameter
+    {
+        set
+        {
+            slider.value = value;
+            ParameterValue = value;
+
+            animator.SetFloat(ParameterName, ParameterValue);
+        }
+        get => ParameterValue;
+    }
+
+    private VRCPlayerApi LocalPlayer;
+
+    private void Start()
+    {
+        LocalPlayer = Networking.LocalPlayer;
+    }
+
+    public void OnValueChanged()
+    {
+        Networking.SetOwner(LocalPlayer, gameObject);
+
+        ParameterValue = slider.value;
+        animator.SetFloat(ParameterName, ParameterValue);
+        RequestSerialization();
+    }
+
+
+}
